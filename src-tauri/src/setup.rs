8f475c30fb97f3978ff84553eba61faef378f2ac
SinanGentoo <sinangentoo@gmail.com>
2023-01-09 14:10:28 +0800

    🔧 Fix conditional compiling

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -3,29 +3,38 @@ use window_vibrancy::{self, NSVisualEffectMaterial};
 
 use window_shadows::set_shadow;
 
-/// setup
+#[cfg(target_os = "macos")]
 pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let win = app.get_window("main").unwrap();
-
-    #[cfg(any(windows, target_os = "macos"))]
     set_shadow(&win, true).unwrap();
 
-    #[cfg(target_os = "macos")]
     window_vibrancy::apply_vibrancy(&win, NSVisualEffectMaterial::FullScreenUI)
         .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
-    #[cfg(target_os = "windows")]
-    use os_info::Version;
+    Ok(())
+}
 
+#[cfg(target_os = "windows")]
+pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let win = app.get_window("main").unwrap();
+
+    set_shadow(&win, true).unwrap();
+
+    use os_info::Version;
     let binding = os_info::get();
     let build = binding.version();
     if PartialOrd::ge(&build, &&Version::Semantic(10, 0, 21996)) {
         window_vibrancy::apply_mica(&win)
-            .expect("Unsupported platform! 'apply_mica' is only supported on Windows 11");
+            .expect("Unsupported platform! 'apply_mica' is only supported on Windows 10.0.21996");
     } else if PartialOrd::ge(&build, &&Version::Semantic(10, 0, 17134)) {
         window_vibrancy::apply_acrylic(&win, Some((18, 18, 18, 125)))
-            .expect("Unsupported platform! 'apply_arcylic' is only supported on Windows");
+            .expect("Unsupported platform! 'apply_arcylic' is only supported on Windows 10.0.17134");
     }
 
+    Ok(())
+}
+
+#[cfg(target_os = "linux")]
+pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
